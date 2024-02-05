88e94d7ba20aa005d76f035d64f68ca216d2fdf8
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-02-05 19:19:47 +0530

    changes to server

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 import socket
 import threading
-from BplusTree import tree
+from bplustree import BPlusTree
 from server_operations import ServerOps
+import queue
 import time
-def handle_client(client_socket,serverOperator:ServerOps):
+def handle_client(client_socket,filename):
+    tree=BPlusTree(f"./TestDB/{filename}")
+    serverOperator=ServerOps(tree)
     # Receive data from the client
-    data = client_socket.recv(1024)
+
+    data = client_socket.recv(4096)
     # Send data back to the client
     data=serverOperator.retrieve_merchants(int(data.decode("utf-8")))
-
     client_socket.sendall(data.encode("utf-8"))
-
-   
-
+    tree.close()
     # Close the connection
     client_socket.close()
 # Create a socket object
@@ -24,12 +25,11 @@ server_socket.bind(server_address)
 
 # Listen for incoming connections
 server_socket.listen(5)
-serverOperator=ServerOps(tree)
+
 print('Server is listening on {}:{}'.format(*server_address))
 
 while True:
     client_socket, client_address = server_socket.accept()
-
     # Handle the client connection in a separate thread
-    threading.Thread(target=handle_client, args=(client_socket,serverOperator)).start()
+    threading.Thread(target=handle_client, args=(client_socket,"local.db")).start()
     
