fbb73ceb1bf2d5ae1896ae4cb3483e5bf9579665
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-02-09 10:18:19 +0530

    changes to server.py

--- server.py
+++ server.py
@@ -8,7 +8,13 @@ def decode_merchant_data(byte_data:bytes) -> dict:
     string_data = byte_data.decode('utf-8')
     parts = string_data.strip().split()
     merchant_id = parts[0]
-    pincodes =  [int(code) for code in parts[1:] if code>=100000 and code<=999999 else throw _error("Invalid pincode") ]
+    pincodes = []
+    for code in parts[1:]:
+        if code.isdigit() and 100000<=int(code)<=999999:
+            pincodes.append(int(code))
+        else:
+            raise ValueError("Invalid pincode: {}".format(code))
+
     data_dict = {merchant_id : pincodes}
     
 async def handle_client(reader, writer,filename):
@@ -26,12 +32,16 @@ async def handle_client(reader, writer,filename):
     await writer.drain()
     writer.close()
     
-async def handle_superUser(reader,writer,filename):
+async def handle_superUser(reader,writer):
     
     byte_data=await reader.read(4096)
     filename = await reader.read(4096)
-    merchants_dict = decode_merchant_data(byte_data)  # Decode the received dict from bytes to string and then to dictionary
-    data="abcd"
+    try:
+        merchants_dict = decode_merchant_data(byte_data) 
+        data=""
+    except ValueError as e:
+        data=e+" : Pincode should be 6 digits" # Decode the received dict from bytes to string and then to dictionary
+   
     data=data.encode()
     writer.write(data)
     await writer.drain()
@@ -60,8 +70,8 @@ async def main():
 
     # Start both servers concurrently
     await asyncio.gather(
-        start_server1(server1_host, server1_port,"local.db"),
-        start_server2(server2_host, server2_port,"local.db")
+        start_server1(server1_host, server1_port,"merchant.db"),
+        start_server2(server2_host, server2_port,"merchant.db")
     )
 
 asyncio.run(main())
