79c55687e43a8bb4969d1532ce612e4f4dfd335f
Shreyas529 <122379271+Shreyas529@users.noreply.github.com>
2024-09-04 14:06:18 +0530

    Delete virtenv directory

--- virtenv/lib/python3.8/site-packages/bplustree/__init__.py
+++
@@ -1,7 +0,0 @@
-from .tree import BPlusTree
-from .serializer import (
-    IntSerializer, StrSerializer, UUIDSerializer, DatetimeUTCSerializer
-)
-from .const import VERSION
-
-__version__ = VERSION
